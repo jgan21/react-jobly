50b9fdeae177dc8a251e7fef67085ef4261cd892
Brandie Lucano <blucano.dev@gmail.com>
2024-01-23 16:22:07 -0800

    added Nav and RoutesList component. Structured App component.

---
+++ src/RoutesList.js
@@ -0,0 +1,27 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+// TODO:import components
+
+/** List of routes for Jobly
+ *
+ * Props: None
+ *
+ * State: None
+ *
+ * App -> RoutesList
+*/
+function RoutesList(){
+
+  return(
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="/companies" element={<CompanyList/>}/>
+        <Route path="/companies/:handle" element={<CompanyDetails/>}/>
+        <Route path="/jobs" element={<JobList />}/>
+        <Route path="*" element={<Navigate to="/"/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default RoutesList;
