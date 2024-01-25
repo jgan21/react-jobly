dfae2078c911be674dee1a72878d8d094099de6a
Brandie Lucano <blucano.dev@gmail.com>
2024-01-25 14:22:54 -0800

    updated routes list to include user routes and props from App

--- src/RoutesList.js
+++ src/RoutesList.js
@@ -3,18 +3,25 @@ import Homepage from "./Homepage";
 import CompanyList from "./CompanyList";
 import CompanyDetails from "./CompanyDetails";
 import JobList from "./JobList";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+import ProfileForm from "./ProfileForm";
+
 
 
 /** List of routes for Jobly
  *
- * Props: None
+ * Props:
+ * -login(): function to call in parent to login user
+ * -signup(): function to call in parent to signup a new user
+ * -editProfile(): function to call in parent to update user profile
  *
  * State: None
  *
  * App -> RoutesList
 */
 
-function RoutesList(){
+function RoutesList({ login, signup, editProfile }){
 
   return(
     <div>
@@ -23,6 +30,9 @@ function RoutesList(){
         <Route path="/companies" element={<CompanyList/>}/>
         <Route path="/companies/:handle" element={<CompanyDetails/>}/>
         <Route path="/jobs" element={<JobList />}/>
+        <Route path="/login" element={<LoginForm login={login}/>}/>
+        <Route path="/signup" element={<SignupForm signup={signup}/>}/>
+        <Route path="/profile" element={<ProfileForm editProfile={editProfile}/>}/>
         <Route path="*" element={<Navigate to="/"/>}/>
       </Routes>
     </div>
