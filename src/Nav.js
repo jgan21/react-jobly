be49089f51d330057840e6adca626a9136fab954
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-25 17:53:52 -0600

    added conditional logic to Nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,25 +1,42 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import "./Nav.css"
+import "./Nav.css";
+import userContext from "./userContext";
 
 /** Nav: links to each endpoint.
  *
  * State: None
  *
- * Props: None
+ * Props:
+ * - logout() : function to call in parent(App)
  *
- * App -> Nav: links to {"/", "/companies", "/jobs" }
+ * App -> Nav
  */
 
-function Nav() {
+function Nav({ logout }) {
+  // const { currUser } = useContext(userContext);
 
-  return (
-    <nav className="Nav">
-      <NavLink to="/">Jobly</NavLink>
-      <NavLink className="Nav-right" to="/companies">Companies</NavLink>
-      <NavLink className="Nav-right" to="/jobs">Jobs</NavLink>
-    </nav>
-  );
-}
-
+  // if (currUser) {
+  //   return (
+  //     <nav className="Nav">
+  //       <NavLink className="Nav-right" to="/companies">Companies</NavLink>
+  //       <NavLink className="Nav-right" to="/jobs">Jobs</NavLink>
+  //       <NavLink className="Nav-right" to="/profile">Profile</NavLink>
+  //     //TODO:
+  //       <NavLink className="Nav-right" to="/logout" logout={logout}>
+  //         Logout {`${currUser.firstName}`}
+  //       </NavLink>
+  //     </nav>
+  //   );
+  // } else {
+    return (
+      <nav className="Nav">
+        <NavLink to="/">Jobly</NavLink>
+        <NavLink className="Nav-right" to="/login">Login</NavLink>
+        <NavLink className="Nav-right" to="/signup">Sign Up</NavLink>
+      </nav>
+    );
+  }
+// }
 
 export default Nav;
