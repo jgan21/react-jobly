5bee4965722c0714840d9d7a47c8ef2cad088fe3
Brandie Lucano <blucano.dev@gmail.com>
2024-01-25 14:55:08 -0800

    added pseudocode for handling auth functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import{ BrowserRouter } from "react-router-dom";
 import Nav from "./Nav";
 import RoutesList from "./RoutesList";
+import userContext from "./userContext";
 
 /**App for Jobly
  *
@@ -12,13 +13,42 @@ import RoutesList from "./RoutesList";
  */
 
 function App() {
+  //state for: keeping track of user info
+  //state for  token: use an effect to watch for changes to the token to kick off
+    //a process of loading the information about the new user.
+
+  //useEffect: dependency of token
+    //if token changes,
+
+
+
+
+  //async login()
+    //-call JoblyApi function to get token
+    //catch errors-alert box
+    //store token in state
+    //update user context by decoding jwt
+
+
+  //async signup()
+    //-call JoblyApi function to get token
+    //catch errors-alert box
+    //store token in state
+    //update user context by decoding jwt
+
+  // logout()
+    //set user context to null
+    //set token to null
+
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav />
-        <RoutesList />
+        <userContext.Provider>
+          <Nav />
+          <RoutesList />
+        </userContext.Provider>
       </BrowserRouter>
-
     </div>
   );
 }
